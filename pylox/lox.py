d4dd0e00109b1bb63a24da28e4c748bec6c1852e
Edward Amor <edward.amor3@gmail.com>
2022-05-29 12:05:34 +0000

    feat: add error + report functions to lox.py

--- pylox/lox.py
+++ pylox/lox.py
@@ -1,19 +1,48 @@
+import sys
+
 import click
+
+from pylox.scanner import Scanner
+
+HAD_ERROR = False  # noqa: F841
 
 
 @click.command()
 @click.argument("file", type=click.File(), required=False)
-def main(file):
+def main(file: click.File):
     # entrypoint for the pylox interpreter
     if file is None:
         # interactive interpreter
         while True:
+            # header message
+            click.echo("Pylox 0.1.0 Interactive Prompt")
             # run each line
             run(input("> "))
     else:
         # execute entire file
         run(file.read())
+        if HAD_ERROR:
+            # exit with a non-zero exit code
+            sys.exit(65)
 
 
-def run(source):
-    pass
+def run(source: str):
+    # scan the source code
+    scanner = Scanner(source)
+    # collect all the tokens in a list
+    tokens = scanner.scan_tokens()
+
+    # print tokens to the console
+    for token in tokens:
+        click.echo(token)
+
+
+def error(line: int, message: str):
+    # error utility function
+    report(line, "", message)
+
+
+def report(line: int, where: str, message: str):
+    # report error to stderr
+    click.echo(f"[line {line}] Error {where}: {message}", err=True)
+    HAD_ERROR = True
