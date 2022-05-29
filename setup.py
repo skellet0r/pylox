41bc939d36b91ead3a53541a9063e9720ff37b41
Edward Amor <edward.amor3@gmail.com>
2022-05-29 09:53:25 +0000

    fix: add install requires to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from setuptools import find_packages, setup
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
+requirements = list(map(str.strip, (here / "requirements.txt").read_text().split("\n")))[:-1]
+
 setup(
     name="pylox",
     version="0.1.0",
@@ -14,4 +16,10 @@ setup(
     url="https://github.com/skellet0r/pylox",
     packages=find_packages(),
     python_requires=">=3.10, <4",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "pylox = pylox.lox:main",
+        ],
+    },
 )
