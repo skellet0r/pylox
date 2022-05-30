51a28578c32408ea4a994231dbbbb444818d05cb
Edward Amor <edward.amor3@gmail.com>
2022-05-30 18:53:32 +0000

    feat: handle strings in lexer

--- pylox/lexer.py
+++ pylox/lexer.py
@@ -1,12 +1,14 @@
+from pylox.exceptions import ExceptionList, LexicalError
 from pylox.token import Token, TokenType
 
 
 class Lexer:
     """Scan source code capturing valid tokens."""
 
-    def __init__(self, source: str):
+    def __init__(self, source: str, exception_list: ExceptionList):
         self.source = source
         self.tokens: list[Token] = []
+        self.exception_list = exception_list
 
         self._current = 0
         self._start = 0
@@ -16,7 +18,11 @@ class Lexer:
         # loop through source scanning for tokens
         while self._current < len(self.source):
             self._start = self._current
-            self.scan_token()
+            try:
+                self.scan_token()
+            except LexicalError as e:
+                self.exception_list.append(e)
+                self._current += 1
 
         # the last token should be the EOF
         if len(self.tokens) != 0 and self.tokens[-1].token_type is not TokenType.EOF:
@@ -95,6 +101,26 @@ class Lexer:
                         self._current += 1
                 else:
                     self.add_token(TokenType.SLASH)
+            case '"':
+                # string is a sequence of chars between double quotes
+                while (next_char := self.peek()) not in ['"', "\0"]:
+                    # peek at the char under the cursor if it isn't EOF or end '"'
+                    # we increment the cursor (also handle newline chars)
+                    if next_char == "\n":
+                        self._lineno += 1
+                    self._current += 1
+
+                # we've consumed all the chars, check that the char under the cursor
+                # isn't an EOF, which would mean that the string is unterminated
+                if self.peek() == "\0":
+                    # handle unterminated string
+                    # throw an error
+                    raise LexicalError(self._lineno, "", "Unterminated string")
+
+                # increment the cursor, and add the string token to list of tokens
+                self._current += 1
+                value = self.source[self._start + 1 : self._current - 1]
+                self.add_token(TokenType.STRING, value)
 
     def consume(self) -> str:
         char = self.source[self._current]
