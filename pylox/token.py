dbb3e97d852ea1e241b9cb21750337d4389604b3
Edward Amor <edward.amor3@gmail.com>
2022-05-30 23:55:30 +0000

    fix: use dataclass for token class

--- pylox/token.py
+++ pylox/token.py
@@ -1,18 +1,5 @@
+import dataclasses
 import enum
-
-
-class Token:
-    def __init__(self, token_type: "TokenType", lexeme: str, literal: object, lineno: int):
-        # raw substring from the source code
-        # smallest sequence of characters that represent something
-        self.token_type = token_type
-        self.lexeme = lexeme
-        self.literal = literal
-        self.lineno = lineno
-
-    def __repr__(self) -> str:
-        return f"Token({self.token_type}, '{self.lexeme}', {self.literal}, {self.lineno})"
-
 
 ONE_CHAR_TOKENS = (
     "LEFT_BRACE",
@@ -63,4 +50,13 @@ RESERVED_TOKENS = (
 EOF_TOKEN = ("EOF",)
 ALL_TOKENS = ONE_CHAR_TOKENS + ONE_OR_TWO_CHAR_TOKENS + LITERAL_TOKENS + RESERVED_TOKENS + EOF_TOKEN
 
+
 TokenType = enum.Enum("TokenType", ALL_TOKENS)
+
+
+@dataclasses.dataclass
+class Token:
+    token_type: TokenType
+    lexeme: str
+    literal: object
+    lineno: int
