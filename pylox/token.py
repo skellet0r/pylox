41f6a624f824c1e4ac73036bb375987ee0259cf7
Edward Amor <edward.amor3@gmail.com>
2022-05-30 13:18:42 +0000

    fix: refactor token module

--- pylox/token.py
+++ pylox/token.py
@@ -1,63 +1,66 @@
-from enum import Enum, auto
-
-
-class TokenType(Enum):
-    # single character tokens
-    LEFT_PAREN = auto()
-    RIGHT_PAREN = auto()
-    LEFT_BRACE = auto()
-    RIGHT_BRACE = auto()
-    COMMA = auto()
-    DOT = auto()
-    MINUS = auto()
-    PLUS = auto()
-    SEMICOLON = auto()
-    SLASH = auto()
-    STAR = auto()
-
-    # one or two character tokens
-    BANG = auto()
-    BANG_EQUAL = auto()
-    EQUAL = auto()
-    EQUAL_EQUAL = auto()
-    GREATER = auto()
-    GREATER_EQUAL = auto()
-    LESS = auto()
-    LESS_EQUAL = auto()
-
-    # literals
-    IDENTIFIER = auto()
-    STRING = auto()
-    NUMBER = auto()
-
-    # keywords
-    AND = auto()
-    CLASS = auto()
-    ELSE = auto()
-    FALSE = auto()
-    FUN = auto()
-    FOR = auto()
-    IF = auto()
-    NIL = auto()
-    OR = auto()
-    PRINT = auto()
-    RETURN = auto()
-    SUPER = auto()
-    TRUE = auto()
-    VAR = auto()
-    WHILE = auto()
-    THIS = auto()
-
-    EOF = auto()
+import enum
 
 
 class Token:
-    def __init__(self, type_: TokenType, lexeme: str, literal: object, line: int):
-        self.type_ = type_
-        # lexeme: a sequence of chars that grouped together represent something
+    def __init__(self, token_type: "TokenType", lexeme: str, literal: object, lineno: int):
+        # raw substring from the source code
+        # smallest sequence of characters that represent something
+        self.token_type = token_type
         self.lexeme = lexeme
         self.literal = literal
-        self.line = line
+        self.lineno = lineno
 
-    def __str__(self) -> str:
-        return f"{self.type_} {self.lexeme} {self.literal}"
+    def __repr__(self) -> str:
+        return f"Token({self.token_type}, '{self.lexeme}', {self.literal}, {self.lineno})"
+
+
+ONE_CHAR_TOKENS = (
+    "LEFT_BRACE",
+    "RIGHT_BRACE",
+    "LEFT_PAREN",
+    "RIGHT_PAREN",
+    "COMMA",
+    "DOT",
+    "MINUS",
+    "PLUS",
+    "SEMICOLON",
+    "SLASH",
+    "STAR",
+)
+ONE_OR_TWO_CHAR_TOKENS = (
+    "BANG",
+    "BANG_EQUAL",
+    "EQUAL",
+    "EQUAL_EQUAL",
+    "GREATER",
+    "GREATER_EQUAL",
+    "LESS",
+    "LESS_EQUAL",
+)
+LITERAL_TOKENS = (
+    "IDENTIFIER",
+    "NUMBER",
+    "STRING",
+)
+RESERVED_TOKENS = (
+    "AND",
+    "CLASS",
+    "ELSE",
+    "FALSE",
+    "FOR",
+    "FUN",
+    "IF",
+    "NIL",
+    "OR",
+    "PRINT",
+    "RETURN",
+    "SUPER",
+    "THIS",
+    "TRUE",
+    "VAR",
+    "WHILE",
+)
+EOF_TOKEN = ("EOF",)
+ALL_TOKENS = ONE_CHAR_TOKENS + ONE_OR_TWO_CHAR_TOKENS + LITERAL_TOKENS + RESERVED_TOKENS + EOF_TOKEN
+
+TokenType = enum.Enum("TokenType", ALL_TOKENS)
