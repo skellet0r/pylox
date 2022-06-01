fabe8768d7a0f148bf122d92502cac90270607a5
Edward Amor <edward.amor3@gmail.com>
2022-06-01 21:53:41 +0000

    chore: modify import in visitor.py

--- pylox/visitor.py
+++ pylox/visitor.py
@@ -1,23 +1,23 @@
-from pylox import expr
+from pylox.expr import BaseExpr, BinaryExpr, GroupingExpr, LiteralExpr, UnaryExpr
 
 
 class ASTPrinter:
-    def print(self, expr: expr.BaseExpr) -> str:
+    def print(self, expr: BaseExpr) -> str:
         return expr.accept(self)
 
-    def visitBinaryExpr(self, expr: expr.BinaryExpr) -> str:
+    def visitBinaryExpr(self, expr: BinaryExpr) -> str:
         return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
 
-    def visitGroupingExpr(self, expr: expr.GroupingExpr) -> str:
+    def visitGroupingExpr(self, expr: GroupingExpr) -> str:
         return self.parenthesize("group", expr.expression)
 
-    def visitLiteralExpr(self, expr: expr.LiteralExpr) -> str:
+    def visitLiteralExpr(self, expr: LiteralExpr) -> str:
         if expr.value is None:
             return "nil"
         return str(expr.value)
 
-    def visitUnaryExpr(self, expr: expr.UnaryExpr) -> str:
+    def visitUnaryExpr(self, expr: UnaryExpr) -> str:
         return self.parenthesize(expr.operator.lexeme, expr.right)
 
-    def parenthesize(self, name: str, *exprs: list[expr.BaseExpr]) -> str:
+    def parenthesize(self, name: str, *exprs: list[BaseExpr]) -> str:
         return f"({name} {' '.join((expr.accept(self) for expr in exprs))})"
